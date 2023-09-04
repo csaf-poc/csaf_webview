c047a17839047bebb6f17b300158f9611eca774e
Thomas Junk <thomas.junk@intevation.de>
2023-09-04 16:15:20 +0200

    feat: Add Vulnerabilies CVE, CWE

--- src/lib/Collapsible.svelte
+++ src/lib/Collapsible.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   export let header: string;
   export let open: boolean = true;
+  export let index: string = "2";
+  export let class_: string = "";
   let visibility = open ? "block" : "none";
   let icon = "bx-chevron-down";
   const toggle = (e: Event) => {
@@ -17,9 +19,21 @@
 </script>
 
 <div>
-  <!-- svelte-ignore a11y-click-events-have-key-events -->
-  <!-- svelte-ignore a11y-no-static-element-interactions -->
-  <div on:click={toggle}><h2><i class="bx {icon}" />{header}</h2></div>
+  {#if index == "2"}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <div on:click={toggle} class={class_}><h2><i class="bx {icon}" />{header}</h2></div>
+  {/if}
+  {#if index == "3"}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <div on:click={toggle} class={class_}><h3><i class="bx {icon}" />{header}</h3></div>
+  {/if}
+  {#if index == "4"}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <div on:click={toggle} class={class_}><h4><i class="bx {icon}" />{header}</h4></div>
+  {/if}
   <div style="display:{visibility}" class="body">
     <slot />
   </div>
--- src/lib/singleview/vulnerabilities/Vulnerabilities.svelte
+++ src/lib/singleview/vulnerabilities/Vulnerabilities.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
   import { appStore } from "$lib/store";
+  import Vulnerability from "./Vulnerability.svelte";
+  $: vulnerabilities = $appStore.doc?.vulnerabilities;
 </script>
 
-<div />
+<div>
+  {#each vulnerabilities as vulnerability, index}
+    <Vulnerability {vulnerability} index={index + 1} />
+  {/each}
+</div>
