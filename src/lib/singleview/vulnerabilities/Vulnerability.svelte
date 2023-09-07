5722caa5371c52cdf0f54024c3f45ec311555db9
Thomas Junk <thomas.junk@intevation.de>
2023-09-07 12:37:15 +0200

    chore: Add missing licensing info

--- src/lib/singleview/vulnerabilities/Vulnerability.svelte
+++ src/lib/singleview/vulnerabilities/Vulnerability.svelte
@@ -1,3 +1,12 @@
+<!--
+ This file is Free Software under the MIT License
+ without warranty, see README.md and LICENSES/MIT.txt for details.
+
+ SPDX-License-Identifier: MIT
+
+ SPDX-FileCopyrightText: 2023 German Federal Office for Information Security (BSI) <https://www.bsi.bund.de>
+ Software-Engineering: 2023 Intevation GmbH <https://intevation.de
+-->
 <script lang="ts">
   import Collapsible from "$lib/Collapsible.svelte";
   import { appStore } from "$lib/store";
