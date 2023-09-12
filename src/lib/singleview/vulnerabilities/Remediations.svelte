01b9700fc17b4888ed267d60979d88231ffa5524
Thomas Junk <thomas.junk@intevation.de>
2023-09-12 10:56:46 +0200

    feat: Add remediations

---
+++ src/lib/singleview/vulnerabilities/Remediations.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Collapsible from "$lib/Collapsible.svelte";
+  export let vulnerability: any;
+</script>
+
+<Collapsible header="Remediations" level="4">
+  {#each vulnerability.remediations as remediation}
+    <dl>
+      <dt>Category</dt>
+      <dd>{remediation.category}</dd>
+      <dt>Details</dt>
+      <dd>{remediation.details}</dd>
+    </dl>
+  {/each}
+</Collapsible>
+
+<style>
+  dt {
+    float: left;
+    clear: left;
+    font-weight: 100;
+    width: 14rem;
+  }
+  dd {
+    margin-bottom: 0.1em;
+  }
+</style>
