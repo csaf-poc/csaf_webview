246c9162996fe296f2a43f2deb70a666a431f59a
Thomas Junk <thomas.junk@intevation.de>
2023-09-13 16:15:54 +0200

    refactor: Vulnerabilities

---
+++ src/lib/singleview/ValueField.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let value = "";
+  export let label = "";
+</script>
+
+<div class="">
+  <h6>{label}</h6>
+  <p>{value}</p>
+</div>
+
+<style>
+  h6 {
+    line-height: 0.3em;
+    font-size: large;
+  }
+  p {
+    margin-left: 4rem;
+    text-align: justify;
+  }
+</style>
---
+++ src/lib/singleview/ValueList.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let values = "";
+  export let label = "";
+</script>
+
+<h6>{label}</h6>
+<ul>
+  {#each values as value}
+    <li>{value}</li>
+  {/each}
+</ul>
+
+<style>
+  h6 {
+    line-height: 0.3em;
+    font-size: large;
+  }
+  ul {
+    list-style-type: none;
+  }
+</style>
