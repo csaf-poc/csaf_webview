ee0b223da7d125e9aafd31982cea91db6468a429
Thomas Junk <thomas.junk@intevation.de>
2023-09-27 17:11:34 +0200

    feat: Proxy feature added

--- src/lib/feedview/Loader.svelte
+++ src/lib/feedview/Loader.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
   import { appStore } from "$lib/store";
   let url = "";
+  let proxyURL = "";
   let disabled = true;
+  let proxy = false;
+  $: proxyInputVisible = proxy ? "display: block;" : "display: none;";
   $: if (/^http/.test(url)) {
     disabled = false;
   }
   async function loadProviderMetaData() {
-    const response = await fetch(`${url}`);
+    let targetURL = url;
+    if (proxy && proxyURL) {
+      targetURL = `${proxyURL}${url}`;
+    }
+    const response = await fetch(`${targetURL}`);
     const providerMetadata = await response.json();
     appStore.setProviderMetadata(providerMetadata);
   }
@@ -29,8 +36,32 @@
     </div>
   </div>
 </div>
+<div class="row">
+  <div class="col">
+    <div style="display:flex">
+      <div class="proxychoice">
+        <input id="proxy" type="checkbox" bind:checked={proxy} />
+        <label for="proxy">Use Proxy</label>
+      </div>
+      <div class="proxyurl" style={proxyInputVisible}>
+        <input type="text" bind:value={proxyURL} />
+      </div>
+    </div>
+  </div>
+</div>
 
 <style>
+  .proxychoice {
+    width: 200px;
+    height: 50px;
+    font-size: large;
+  }
+  .proxyurl {
+    width: 100%;
+    height: 50px;
+    font-size: large;
+    margin-left: 1.5rem;
+  }
   .loadbutton {
     width: 200px;
     height: 50px;
