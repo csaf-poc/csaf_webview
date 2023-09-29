e4adb81491a91ddc009706da5d4a93db930153a4
Thomas Junk <thomas.junk@intevation.de>
2023-09-29 10:22:11 +0200

    feat: add display of errors

--- src/lib/feedview/Loader.svelte
+++ src/lib/feedview/Loader.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
   import { appStore } from "$lib/store";
   let url = "";
+  let errorMsg = "";
   async function loadProviderMetaData() {
-    const response = await fetch(`${url}`);
-    const providerMetadata = await response.json();
-    appStore.setProviderMetadata(providerMetadata);
+    errorMsg = "";
+    appStore.reset();
+    try {
+      const response = await fetch(`${url}`);
+      if (response.ok) {
+        const providerMetadata = await response.json();
+        appStore.setProviderMetadata(providerMetadata);
+      }
+      if (response.status === 404) {
+        errorMsg = "The resource you requested was not found on the server.";
+      }
+    } catch (error) {
+      errorMsg =
+        "Failed to load from URL. The server may be unreachable or the resource cannot be accessed due to CORS restrictions.";
+    }
   }
+
   const load = () => {
     loadProviderMetaData();
   };
@@ -23,8 +37,16 @@
     </div>
   </div>
 </div>
+<div class="row">
+  <div class="col"><div class="errors text-error">{errorMsg}</div></div>
+</div>
 
 <style>
+  .errors {
+    margin-left: 200px;
+    font-size: x-large;
+    font-weight: bold;
+  }
   .loadbutton {
     width: 200px;
     height: 50px;
