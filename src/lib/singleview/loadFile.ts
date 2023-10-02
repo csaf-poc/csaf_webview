798ba0a026a6a642e0ff4b248a99154e4672e006
Thomas Junk <thomas.junk@intevation.de>
2023-10-02 11:32:29 +0200

    chore: Add licenses

--- src/lib/singleview/loadFile.ts
+++ src/lib/singleview/loadFile.ts
@@ -1,8 +1,12 @@
+// This file is Free Software under the MIT License
+// without warranty, see README.md and LICENSES/MIT.txt for details.
+//
+// SPDX-License-Identifier: MIT
+//
+// SPDX-FileCopyrightText: 2023 German Federal Office for Information Security (BSI) <https://www.bsi.bund.de>
+// Software-Engineering: 2023 Intevation GmbH <https://intevation.de>
+
 import { convertToDocModel } from "$lib/singleview/docmodel/docmodel";
-import {
-  extractProducts,
-  generateProductVulnerabilities
-} from "./productvulnerabilities/productvulnerabilities";
 import { appStore } from "$lib/store";
 const loadFile = (csafFile: File) => {
   const fileReader: FileReader = new FileReader();
