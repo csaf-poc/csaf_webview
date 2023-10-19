e31944433f978ae80cc1d2504cc7ae23b1cdc1c6
Thomas Junk <thomas.junk@intevation.de>
2023-10-19 12:14:04 +0200

    refactor: extract EntryByYear to separate collapsible

---
+++ src/lib/URLCollapsible.svelte
@@ -0,0 +1,85 @@
+<!--
+ This file is Free Software under the MIT License
+ without warranty, see README.md and LICENSES/MIT.txt for details.
+
+ SPDX-License-Identifier: MIT
+
+ SPDX-FileCopyrightText: 2023 German Federal Office for Information Security (BSI) <https://www.bsi.bund.de>
+ Software-Engineering: 2023 Intevation GmbH <https://intevation.de>
+-->
+
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  export let header: string;
+  export let open = false;
+  export let level = "2";
+  export let class_ = "";
+  export let highlight = false;
+  export let entry: any;
+  export let lookupID: string;
+  export let onClose = () => {
+    //default: Do notthing
+  };
+  let visibility = "none";
+  $: if (open) {
+    visibility = "block";
+  }
+  let icon = "bx-chevron-down";
+  const openLink = (e: Event) => {
+    let url: string = (e.target as Element).getAttribute("href")!;
+    goto(`/?q=${url}`);
+    e.preventDefault();
+  };
+  const toggle = () => {
+    if (visibility === "block") {
+      onClose();
+      visibility = "none";
+    } else {
+      visibility = "block";
+    }
+  };
+  $: if (visibility === "block") {
+    icon = "bx-chevron-down";
+  } else {
+    icon = "bx-chevron-right";
+  }
+</script>
+
+<div class:collapsible={true} class:bg-light={highlight}>
+  {#if level == "4"}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <div style="cursor:pointer" id={header} on:click={toggle} class={class_}>
+      <h4>
+        <i class="bx {icon}" />
+        <a href={lookupID} on:click={openLink}>{entry.id}: {entry.title}</a>
+      </h4>
+    </div>
+  {/if}
+  <div style="display:{visibility}" class="body">
+    <slot />
+  </div>
+</div>
+
+<style>
+  .body {
+    padding-left: 3rem;
+  }
+  h2,
+  h3,
+  h4 {
+    margin: 0;
+  }
+  h3,
+  h4 {
+    line-height: 3rem;
+  }
+  h2 {
+    font-weight: bold;
+  }
+  h6 {
+    line-height: 0.3em;
+    font-size: large;
+    margin: 0.6rem;
+  }
+</style>
--- src/lib/feedview/entries/EntriesByYear.svelte
+++ src/lib/feedview/entries/EntriesByYear.svelte
@@ -9,64 +9,23 @@
 -->
 
 <script lang="ts">
-  import { goto } from "$app/navigation";
   import Collapsible from "$lib/Collapsible.svelte";
+  import UrlCollapsible from "$lib/URLCollapsible.svelte";
   import Entry from "./Entry.svelte";
   import type { EntryIDURLLookup, EntryType, Link } from "./entrytypes";
   export let entries: EntryType[] = [];
   export let year: string;
-  let visibility = "none";
-  let icon = "bx-chevron-down";
-  const open = (e: Event) => {
-    let url: string = (e.target as Element).getAttribute("href")!;
-    goto(`/?q=${url}`);
-    e.preventDefault();
-  };
-  const toggle = () => {
-    if (visibility === "block") {
-      visibility = "none";
-    } else {
-      visibility = "block";
-    }
-  };
   const entryIDURLLookup = entries.reduce((acc: EntryIDURLLookup, entry: EntryType) => {
     const selfURL = entry.link.find((link: Link) => link.rel === "self");
     acc[entry.id] = selfURL!.href;
     return acc;
   }, {});
-  $: if (visibility === "block") {
-    icon = "bx-chevron-down";
-  } else {
-    icon = "bx-chevron-right";
-  }
 </script>
 
 <Collapsible header={year} level="4">
   {#each entries as entry}
-    <div class:collapsible={true}>
-      <!-- svelte-ignore a11y-click-events-have-key-events -->
-      <!-- svelte-ignore a11y-no-static-element-interactions -->
-      <div style="cursor:pointer">
-        <h4>
-          <i class="bx {icon}" on:click={toggle} /><a
-            href={entryIDURLLookup[entry.id]}
-            on:click={open}>{entry.id}: {entry.title}</a
-          >
-        </h4>
-      </div>
-      <div style="display:{visibility}" class="body">
-        <Entry {entry} />
-      </div>
-    </div>
+    <UrlCollapsible {entry} lookupID={entryIDURLLookup[entry.id]} header={"bananas"} level="4">
+      <Entry {entry} />
+    </UrlCollapsible>
   {/each}
 </Collapsible>
-
-<style>
-  h4 {
-    padding: 0;
-    margin-bottom: 0;
-  }
-  .body {
-    margin-left: 3rem;
-  }
-</style>
