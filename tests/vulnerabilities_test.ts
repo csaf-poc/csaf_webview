582c05f948b11e2e880200d96e458ed799a30050
JanHoefelmeyer <hoefelmeyer.jan@gmail.com>
2023-10-23 14:16:46 +0200

    Add more tests

--- tests/vulnerabilities_test.ts
+++ tests/vulnerabilities_test.ts
@@ -383,3 +383,178 @@ test('Vulnerabilities collapsible CVE entry Product status collapsible entry Kno
   await page.getByRole('heading', { name: ' Product status' }).click();
   await expect(page.getByRole('link', { name: 'CSAF Tools CVRF-CSAF-Converter 1.0.0-alpha' })).toBeVisible(); 
 });
+
+test('Vulnerabilities collapsible CVE entry Product status collapsible entry Known affected value function', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('input[type="file"]').click();
+  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
+
+  await page.getByRole('heading', { name: ' Vulnerabilities', exact:true }).click();  
+  await page.getByRole('heading', { name: ' CVE-2022-27193' }).click();
+  await page.getByRole('heading', { name: ' Product status' }).click();
+  await page.getByRole('link', { name: 'CSAF Tools CVRF-CSAF-Converter 1.0.0-alpha' }).click(); 
+  await expect(page.getByText('CSAF Tools CVRF-CSAF-Converter 1.0.0-alpha Product IDCSAF Tools CVRF-CSAF-Conver')).toBeVisible();
+});
+
+test('Vulnerabilities collapsible CVE entry Product status collapsible entry Known affected last value visibility', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('input[type="file"]').click();
+  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
+
+  await page.getByRole('heading', { name: ' Vulnerabilities', exact:true }).click();  
+  await page.getByRole('heading', { name: ' CVE-2022-27193' }).click();
+  await page.getByRole('heading', { name: ' Product status' }).click();
+  await expect(page.getByRole('link', { name: 'CSAF Tools CVRF-CSAF-Converter 1.0.0-rc1' })).toBeVisible(); 
+});
+
+test('Vulnerabilities collapsible CVE entry Remediations collapsible visibility', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('input[type="file"]').click();
+  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
+
+  await page.getByRole('heading', { name: ' Vulnerabilities', exact:true }).click();  
+  await page.getByRole('heading', { name: ' CVE-2022-27193' }).click();
+  await expect(page.getByRole('heading', { name: ' Remediations' })).toBeVisible();
+});
+
+test('Vulnerabilities collapsible CVE entry Remediations collapsible functionality', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('input[type="file"]').click();
+  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
+
+  await page.getByRole('heading', { name: ' Vulnerabilities', exact:true }).click();  
+  await page.getByRole('heading', { name: ' CVE-2022-27193' }).click();
+  await page.getByRole('heading', { name: ' Remediations' }).click();
+  await expect(page.getByRole('heading', { name: ' Remediations' })).toBeVisible();
+});
+
+test('Vulnerabilities collapsible CVE entry Remediations collapsible category visible', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('input[type="file"]').click();
+  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
+
+  await page.getByRole('heading', { name: ' Vulnerabilities', exact:true }).click();  
+  await page.getByRole('heading', { name: ' CVE-2022-27193' }).click();
+  await page.getByRole('heading', { name: ' Remediations' }).click();
+  await expect(page.getByRole('heading', { name: ' Category: vendor_fix' })).toBeVisible();
+});
+
+test('Vulnerabilities collapsible CVE entry Remediations collapsible category functionality', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('input[type="file"]').click();
+  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
+
+  await page.getByRole('heading', { name: ' Vulnerabilities', exact:true }).click();  
+  await page.getByRole('heading', { name: ' CVE-2022-27193' }).click();
+  await page.getByRole('heading', { name: ' Remediations' }).click();
+  await page.getByRole('heading', { name: ' Category: vendor_fix' }).click();
+  await expect(page.getByRole('heading', { name: ' Category: vendor_fix' })).toBeVisible();    
+});
+
+test('Vulnerabilities collapsible CVE entry Remediations collapsible Category entries visible', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('input[type="file"]').click();
+  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
+
+  await page.getByRole('heading', { name: ' Vulnerabilities', exact:true }).click();  
+  await page.getByRole('heading', { name: ' CVE-2022-27193' }).click();
+  await page.getByRole('heading', { name: ' Remediations' }).click();
+  await page.getByRole('heading', { name: ' Category: vendor_fix' }).click();
+  await expect(page.getByText('Date2022-03-14T13:10:55.000+01:00 URLhttps://github.com/csaf-tools/')).toBeVisible();    
+});
+
+test('Vulnerabilities collapsible CVE entry Remediations collapsible Category Product ID links visible', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('input[type="file"]').click();
+  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
+
+  await page.getByRole('heading', { name: ' Vulnerabilities', exact:true }).click();  
+  await page.getByRole('heading', { name: ' CVE-2022-27193' }).click();
+  await page.getByRole('heading', { name: ' Remediations' }).click();
+  await page.getByRole('heading', { name: ' Category: vendor_fix' }).click();
+  await expect(page.getByRole('link', { name: 'CSAF Tools CVRF-CSAF-Converter 1.0.0-alpha' })).toBeVisible();    
+});
+
+test('Vulnerabilities collapsible CVE entry Remediations collapsible Category Product ID links visible 2', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('input[type="file"]').click();
+  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
+
+  await page.getByRole('heading', { name: ' Vulnerabilities', exact:true }).click();  
+  await page.getByRole('heading', { name: ' CVE-2022-27193' }).click();
+  await page.getByRole('heading', { name: ' Remediations' }).click();
+  await page.getByRole('heading', { name: ' Category: vendor_fix' }).click();
+  await expect(page.getByRole('link', { name: 'CSAF Tools CVRF-CSAF-Converter 1.0.0-rc1' })).toBeVisible();    
+});
+
+// TODO: Possibly fix
+test('Vulnerabilities collapsible CVE entry Remediations collapsible Category Product ID link functionality', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('input[type="file"]').click();
+  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
+
+  await page.getByRole('heading', { name: ' Vulnerabilities', exact:true }).click();  
+  await page.getByRole('heading', { name: ' CVE-2022-27193' }).click();
+  await page.getByRole('heading', { name: ' Remediations' }).click();
+  await page.getByRole('heading', { name: ' Category: vendor_fix' }).click();
+  await page.getByRole('link', { name: 'CSAF Tools CVRF-CSAF-Converter 1.0.0-rc1' }).click;
+  await expect(page.getByRole('link', { name: 'CSAF Tools CVRF-CSAF-Converter 1.0.0-rc1' })).toBeVisible();
+});
+
+test('Vulnerabilities collapsible CVE entry Scores visible', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('input[type="file"]').click();
+  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
+
+  await page.getByRole('heading', { name: ' Vulnerabilities', exact:true }).click();  
+  await page.getByRole('heading', { name: ' CVE-2022-27193' }).click();
+  await expect(page.getByRole('heading', { name: ' Scores' })).toBeVisible();
+});
+
+test('Vulnerabilities collapsible CVE entry Scores functionality', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('input[type="file"]').click();
+  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
+
+  await page.getByRole('heading', { name: ' Vulnerabilities', exact:true }).click();  
+  await page.getByRole('heading', { name: ' CVE-2022-27193' }).click();
+  await page.getByRole('heading', { name: ' Scores' }).click();
+  await expect(page.getByRole('heading', { name: 'Score 1' })).toBeVisible();
+});
+
+test('Vulnerabilities collapsible CVE entry Scores Score functionality', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('input[type="file"]').click();
+  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
+
+  await page.getByRole('heading', { name: ' Vulnerabilities', exact:true }).click();  
+  await page.getByRole('heading', { name: ' CVE-2022-27193' }).click();
+  await page.getByRole('heading', { name: ' Scores' }).click();
+  await page.getByRole('heading', { name: 'Score 1' }).click();
+  await expect(page.getByRole('heading', { name: 'Score 1' })).toBeVisible();
+});
+
+test('Vulnerabilities collapsible CVE entry Scores Score CVSS  visibility', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('input[type="file"]').click();
+  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
+
+  await page.getByRole('heading', { name: ' Vulnerabilities', exact:true }).click();  
+  await page.getByRole('heading', { name: ' CVE-2022-27193' }).click();
+  await page.getByRole('heading', { name: ' Scores' }).click();
+  await page.getByRole('heading', { name: 'Score 1' }).click();
+  await page.getByRole('heading', { name: 'CVSS V3.1' }).click();
+  await expect(page.getByText('attackComplexityLOW attackVectorLOCAL availabilityImpactLOW baseScore6.1 baseSev')).toBeVisible();
+});
+
+test('Vulnerabilities collapsible CVE entry Scores Score ID list visibility', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('input[type="file"]').click();
+  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
+
+  await page.getByRole('heading', { name: ' Vulnerabilities', exact:true }).click();  
+  await page.getByRole('heading', { name: ' CVE-2022-27193' }).click();
+  await page.getByRole('heading', { name: ' Scores' }).click();
+  await page.getByRole('heading', { name: 'Score 1' }).click();
+  await expect(page.getByRole('list')).toBeVisible();
+});
