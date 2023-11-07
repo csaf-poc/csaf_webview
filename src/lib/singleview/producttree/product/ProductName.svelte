52dc9138117709b7b463a6d5161b07a8e9349ee2
Thomas Junk <thomas.junk@intevation.de>
2023-11-07 17:22:00 +0100

    fix: Rewrite Full_Product_Names

---
+++ src/lib/singleview/producttree/product/ProductName.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import ProductIdentificationHelper from "./ProductIdentificationHelper.svelte";
+  import KeyValue from "$lib/KeyValue.svelte";
+  import { appStore } from "$lib/store";
+  import { tick } from "svelte";
+  import Collapsible from "$lib/Collapsible.svelte";
+  export let product: any;
+  async function updateUI() {
+    await tick();
+    document.getElementById(`${product?.product_id}`)?.scrollIntoView({ behavior: "smooth" });
+  }
+  let highlight = false;
+  $: if ($appStore.ui.selectedProduct === product?.product_id) {
+    highlight = true;
+    updateUI();
+  } else {
+    highlight = false;
+  }
+</script>
+
+<div id={product?.product_id}>
+  <Collapsible
+    header={product.name}
+    level="5"
+    {highlight}
+    open={$appStore.ui.selectedProduct === product.product_id}
+    onClose={() => {
+      if ($appStore.ui.selectedProduct === product.product_id) {
+        appStore.resetSelectedProduct();
+      }
+    }}
+  >
+    <KeyValue
+      keys={["Name", "Product ID"]}
+      values={[product.name, product.product_id]}
+      compact={true}
+    />
+    {#if product.product_identification_helper}
+      <ProductIdentificationHelper helper={product.product_identification_helper} />
+    {/if}
+  </Collapsible>
+</div>
--- src/lib/singleview/producttree/product/ProductNames.svelte
+++ src/lib/singleview/producttree/product/ProductNames.svelte
@@ -9,14 +9,11 @@
 -->
 
 <script lang="ts">
-  import ProductIdentificationHelper from "./ProductIdentificationHelper.svelte";
-  import KeyValue from "$lib/KeyValue.svelte";
+  import ProductName from "./ProductName.svelte";
+
   export let productNames: any;
 </script>
 
 {#each productNames as product}
-  <KeyValue keys={["Name", "Product ID"]} values={[product.name, product.product_id]} />
-  {#if product.product_identification_helper}
-    <ProductIdentificationHelper helper={product.product_identification_helper} />
-  {/if}
+  <ProductName {product} />
 {/each}
