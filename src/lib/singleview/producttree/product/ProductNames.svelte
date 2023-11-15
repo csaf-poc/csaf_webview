22b4a4e6885abd1d4085cbd779f30be1f1989132
Thomas Junk <thomas.junk@intevation.de>
2023-11-15 12:36:43 +0100

    feat: sort Full_product_name

--- src/lib/singleview/producttree/product/ProductNames.svelte
+++ src/lib/singleview/producttree/product/ProductNames.svelte
@@ -12,6 +12,11 @@
   import ProductName from "./ProductName.svelte";
 
   export let productNames: any;
+  productNames = productNames.sort((p1: any, p2: any) => {
+    if (p1.name < p2.name) return -1;
+    if (p1.name > p2.name) return 1;
+    return 0;
+  });
 </script>
 
 {#each productNames as product}
