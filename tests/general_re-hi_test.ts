93186c5be07d440c0015cac0203014d00626f09d
Thomas Junk <thomas.junk@intevation.de>
2023-11-24 11:12:59 +0100

    fix integrationtests

--- tests/general_re-hi_test.ts
+++ tests/general_re-hi_test.ts
@@ -11,106 +11,106 @@
 // This part tests the general info revision history collapsible section
 
 import { expect, test } from "@playwright/test";
-import { readFileSync } from "fs";
 
-
-test('general collapsible content Revision history', async ({ page }) => {
-  await page.goto('/');
+test("general collapsible content Revision history", async ({ page }) => {
+  await page.goto("/");
   await page.locator('input[type="file"]').click();
-  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
-  
-  await expect(page.getByRole('heading', { name: ' Revision history' })).toBeVisible()
+  await page.locator('input[type="file"]').setInputFiles("./docs/bsi-2022-0001.json");
+
+  await expect(page.getByRole("heading", { name: " Revision history" })).toBeVisible();
 });
 
-test('general collapsible content Revision history function', async ({ page }) => {
-  await page.goto('/');
+test("general collapsible content Revision history function", async ({ page }) => {
+  await page.goto("/");
   await page.locator('input[type="file"]').click();
-  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
-  
-  await page.getByRole('heading', { name: ' Revision history' }).click();
-  await expect(page.getByRole('heading', { name: ' Revision history' })).toBeVisible()
+  await page.locator('input[type="file"]').setInputFiles("./docs/bsi-2022-0001.json");
+
+  await page.getByRole("heading", { name: " Revision history" }).click();
+  await expect(page.getByRole("heading", { name: " Revision history" })).toBeVisible();
 });
 
-test('general collapsible content Revision history table', async ({ page }) => {
-  await page.goto('/');
+test("general collapsible content Revision history table", async ({ page }) => {
+  await page.goto("/");
   await page.locator('input[type="file"]').click();
-  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
-  
-  await page.getByRole('heading', { name: ' Revision history' }).click();
-  await expect(page.locator('.subsection > .collapsible > .body')).toBeVisible()
+  await page.locator('input[type="file"]').setInputFiles("./docs/bsi-2022-0001.json");
+
+  await page.getByRole("heading", { name: " Revision history" }).click();
+  await expect(page.locator(".subsection > .collapsible > .collapsible-body")).toBeVisible();
 });
 
-
-test('general collapsible content Revision history Date Cell', async ({ page }) => {
-  await page.goto('/');
+test("general collapsible content Revision history Date Cell", async ({ page }) => {
+  await page.goto("/");
   await page.locator('input[type="file"]').click();
-  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
-  
-  await page.getByRole('heading', { name: ' Revision history' }).click();
-  await expect(page.getByRole('cell', { name: 'Date', exact: true })).toBeVisible()
+  await page.locator('input[type="file"]').setInputFiles("./docs/bsi-2022-0001.json");
+
+  await page.getByRole("heading", { name: " Revision history" }).click();
+  await expect(page.getByRole("cell", { name: "Date", exact: true })).toBeVisible();
 });
 
-test('general collapsible content Revision history Date cell value', async ({ page }) => {
-  await page.goto('/');
+test("general collapsible content Revision history Date cell value", async ({ page }) => {
+  await page.goto("/");
   await page.locator('input[type="file"]').click();
-  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
-  
-  await page.getByRole('heading', { name: ' Revision history' }).click();
-  await expect(page.getByRole('cell', { name: '2022-03-17T13:03:42.105Z' }).nth(2)).toBeVisible()
+  await page.locator('input[type="file"]').setInputFiles("./docs/bsi-2022-0001.json");
+
+  await page.getByRole("heading", { name: " Revision history" }).click();
+  await expect(page.getByRole("cell", { name: "2022-03-17T13:03:42.105Z" }).nth(2)).toBeVisible();
 });
 
-test('general collapsible content Revision history Number Cell', async ({ page }) => {
-  await page.goto('/');
+test("general collapsible content Revision history Number Cell", async ({ page }) => {
+  await page.goto("/");
   await page.locator('input[type="file"]').click();
-  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
-  
-  await page.getByRole('heading', { name: ' Revision history' }).click();
-  await expect(page.getByRole('cell', { name: 'Number' })).toBeVisible()
+  await page.locator('input[type="file"]').setInputFiles("./docs/bsi-2022-0001.json");
+
+  await page.getByRole("heading", { name: " Revision history" }).click();
+  await expect(page.getByRole("cell", { name: "Number" })).toBeVisible();
 });
 
-test('general collapsible content Revision history Number cell value', async ({ page }) => {
-  await page.goto('/');
+test("general collapsible content Revision history Number cell value", async ({ page }) => {
+  await page.goto("/");
   await page.locator('input[type="file"]').click();
-  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
-  
-  await page.getByRole('heading', { name: ' Revision history' }).click();
-  await expect(page.getByRole('cell', { name: '1', exact: true }).nth(1)).toBeVisible()
+  await page.locator('input[type="file"]').setInputFiles("./docs/bsi-2022-0001.json");
+
+  await page.getByRole("heading", { name: " Revision history" }).click();
+  await expect(page.getByRole("cell", { name: "1", exact: true }).nth(1)).toBeVisible();
 });
 
-
-test('general collapsible content Revision history Summary Cell', async ({ page }) => {
-  await page.goto('/');
+test("general collapsible content Revision history Summary Cell", async ({ page }) => {
+  await page.goto("/");
   await page.locator('input[type="file"]').click();
-  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
-  
-  await page.getByRole('heading', { name: ' Revision history' }).click();
-  await expect(page.getByRole('cell', { name: 'Summary' })).toBeVisible()
+  await page.locator('input[type="file"]').setInputFiles("./docs/bsi-2022-0001.json");
+
+  await page.getByRole("heading", { name: " Revision history" }).click();
+  await expect(page.getByRole("cell", { name: "Summary" })).toBeVisible();
 });
 
-test('general collapsible content Revision history Summary cell value', async ({ page }) => {
-  await page.goto('/');
+test("general collapsible content Revision history Summary cell value", async ({ page }) => {
+  await page.goto("/");
   await page.locator('input[type="file"]').click();
-  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
-  
-  await page.getByRole('heading', { name: ' Revision history' }).click();
-  await expect(page.getByRole('cell', { name: 'Initial revision' })).toBeVisible()
+  await page.locator('input[type="file"]').setInputFiles("./docs/bsi-2022-0001.json");
+
+  await page.getByRole("heading", { name: " Revision history" }).click();
+  await expect(page.getByRole("cell", { name: "Initial revision" })).toBeVisible();
 });
 
-
-test('general collapsible content Revision history Legacy version cell', async ({ page }) => {
-  await page.goto('/');
+test("general collapsible content Revision history Legacy version cell", async ({ page }) => {
+  await page.goto("/");
   await page.locator('input[type="file"]').click();
-  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
-  
-  await page.getByRole('heading', { name: ' Revision history' }).click();
-  await expect(page.getByRole('cell', { name: 'Legacy_version' })).toBeVisible()
+  await page.locator('input[type="file"]').setInputFiles("./docs/bsi-2022-0001.json");
+
+  await page.getByRole("heading", { name: " Revision history" }).click();
+  await expect(page.getByRole("cell", { name: "Legacy_version" })).toBeVisible();
 });
 
-test('general collapsible content Revision history Legacy version value', async ({ page }) => {
-  await page.goto('/');
+test("general collapsible content Revision history Legacy version value", async ({ page }) => {
+  await page.goto("/");
   await page.locator('input[type="file"]').click();
-  await page.locator('input[type="file"]').setInputFiles('./docs/bsi-2022-0001.json');
-  
-  await page.getByRole('heading', { name: ' Revision history' }).click();
-  await expect(page.getByRole('row', { name: '2022-03-17T13:03:42.105Z 1 Initial revision' }).getByRole('cell').nth(3)).toBeVisible()
+  await page.locator('input[type="file"]').setInputFiles("./docs/bsi-2022-0001.json");
+
+  await page.getByRole("heading", { name: " Revision history" }).click();
+  await expect(
+    page
+      .getByRole("row", { name: "2022-03-17T13:03:42.105Z 1 Initial revision" })
+      .getByRole("cell")
+      .nth(3)
+  ).toBeVisible();
 });
