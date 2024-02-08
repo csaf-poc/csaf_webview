70bf1134cd72216c7bfaad53eefed18f94a8200b
Thomas Junk <thomas.junk@intevation.de>
2024-02-08 16:51:40 +0100

    Add licenseinfo

--- src/lib/urlloader.ts
+++ src/lib/urlloader.ts
@@ -1,3 +1,11 @@
+// This file is Free Software under the MIT License
+// without warranty, see README.md and LICENSES/MIT.txt for details.
+//
+// SPDX-License-Identifier: MIT
+//
+// SPDX-FileCopyrightText: 2023 German Federal Office for Information Security (BSI) <https://www.bsi.bund.de>
+// Software-Engineering: 2023 Intevation GmbH <https://intevation.de>
+
 import { appStore } from "$lib/store";
 import { convertToDocModel } from "$lib/singleview/docmodel/docmodel";
 import { PUBLIC_PROXY_PATH } from "$env/static/public";
